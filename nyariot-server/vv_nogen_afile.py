e5618ba67e99c080d38df348b4331acf907f6999
unSerori <pd.ai.374005475@gmail.com>
2023-07-27 11:35:46 +0900

    ファイルを追加

--- nyariot-server/vv_nogen_afile.py
+++ nyariot-server/vv_nogen_afile.py
@@ -4,33 +4,33 @@ import pyaudio # wavファイルを再生する
 import time # タイムラグをつける
 
 # チャットからもらった値を入れて鯖に投げる
-text = "こんにちは！元気？今日も一日頑張ろうね！"
+def talk_tumugi(text):
+    # 音声合成クエリの作成
+    res1 = requests.post('http://127.0.0.1:50021/audio_query',params = {'text': text, 'speaker': 8})
+    # 音声合成データの作成
+    res2 = requests.post('http://127.0.0.1:50021/synthesis',params = {'speaker': 8},data=json.dumps(res1.json()))
+    # 
+    data = res2.content
 
-# 音声合成クエリの作成
-res1 = requests.post('http://127.0.0.1:50021/audio_query',params = {'text': text, 'speaker': 8})
-# 音声合成データの作成
-res2 = requests.post('http://127.0.0.1:50021/synthesis',params = {'speaker': 8},data=json.dumps(res1.json()))
-# 
-data = res2.content
+    # PyAudioのインスタンスを生成
+    p = pyaudio.PyAudio()
 
-# PyAudioのインスタンスを生成
-p = pyaudio.PyAudio()
+    # ストリームを開く
+    stream = p.open(format=pyaudio.paInt16,  # 16ビット整数で表されるWAVデータ
+                    channels=1,  # モノラル
+                    rate=24000,  # サンプリングレート
+                    output=True)
 
-# ストリームを開く
-stream = p.open(format=pyaudio.paInt16,  # 16ビット整数で表されるWAVデータ
-                channels=1,  # モノラル
-                rate=24000,  # サンプリングレート
-                output=True)
+    # 再生を少し遅らせる（開始時ノイズが入るため）
+    time.sleep(0.2) # 0.2秒遅らせる
 
-# 再生を少し遅らせる（開始時ノイズが入るため）
-time.sleep(0.2) # 0.2秒遅らせる
+    while True:
+        # WAV データを直接再生する
+        stream.write(data)  
 
-# WAV データを直接再生する
-stream.write(data)  
+    # ストリームを閉じる
+    stream.stop_stream()
+    stream.close()
 
-# ストリームを閉じる
-stream.stop_stream()
-stream.close()
-
-# PyAudio のインスタンスを終了する
-p.terminate()
+    # PyAudio のインスタンスを終了する
+    p.terminate()
