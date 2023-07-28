3c6b173d71f1d0f34e088cc2ae976dc7ae1abcae
Rei-Konomiya <reikonomiya56@gmail.com>
2023-07-28 22:05:23 +0900

    RasPi Mic & Share

--- RasberryPi/mic/mik.py
+++ RasberryPi/mic/mik.py
@@ -1,5 +1,6 @@
 import pyaudio
 import wave
+import time
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
@@ -9,32 +10,39 @@ record_secs = 5 # 録音する秒数
 dev_index = 0 # デバイス番号
 wav_output_filename = './audio_file/to_server/micIn.wav' # 出力するファイル
 
-audio = pyaudio.PyAudio() # create pyaudio instantiation
+def record():
+    while True:
+        audio = pyaudio.PyAudio() # create pyaudio instantiation
 
-# create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
-                    input_device_index = dev_index,input = True, \
-                    frames_per_buffer=chunk)
-print("recording")
-frames = []
+        # create pyaudio stream
+        stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                            input_device_index = dev_index,input = True, \
+                            frames_per_buffer=chunk)
+        print("recording")
+        frames = []
+        
+        data = 0
+        
+        #while(data > )
+        # loop through stream and append audio chunks to frame array
+        for i in range(0,int((samp_rate/chunk)*record_secs)):
+            data = stream.read(chunk)
+            frames.append(data)
+            #print(data)
 
-# loop through stream and append audio chunks to frame array
-for i in range(0,int((samp_rate/chunk)*record_secs)):
-    data = stream.read(chunk)
-    frames.append(data)
-    print(data)
+        print("finished recording")
 
-print("finished recording")
+        # stop the stream, close it, and terminate the pyaudio instantiation
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
 
-# stop the stream, close it, and terminate the pyaudio instantiation
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+        # save the audio frames as .wav file
+        wavefile = wave.open(wav_output_filename,'wb')
+        wavefile.setnchannels(chans)
+        wavefile.setsampwidth(audio.get_sample_size(form_1))
+        wavefile.setframerate(samp_rate)
+        wavefile.writeframes(b''.join(frames))
+        wavefile.close()
+    
+        time.sleep(0.5)
